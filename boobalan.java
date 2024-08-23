749068aa2aa9d81e2979c6380db62a1f72e3fbc4
B00balan <boobalan51436@gmail.com>
2024-08-23 08:06:10 +0530

    feat: added a new data

--- boobalan.java
+++ boobalan.java
@@ -6,6 +6,7 @@ class Programmer extends Employee{
  public static void main(String args[]){  
    Programmer p=new Programmer();  
    System.out.println("Programmer salary is:"+p.salary);  
-   System.out.println("Bonus of Programmer is:"+p.bonus);  
+   System.out.println("Bonus of Programmer is:"+p.bonus);
+   system.out.ptintln("Extra bonues of programmer is:"+p.Ertra bonus);
 }  
 }  
